b5a57021a84bab488ac483120e7d83c82d7d826d
Jay Asbury <vbjay.net@gmail.com>
2017-09-03 03:05:25 -0400

    Add webbrowser and set it up

--- Cef Sample/Form1.Designer.vb
+++ Cef Sample/Form1.Designer.vb
@@ -22,9 +22,55 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.pnlBrowser = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pnlBrowser.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlBrowser
+        '
+        Me.pnlBrowser.Controls.Add(Me.Panel1)
+        Me.pnlBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlBrowser.Location = New System.Drawing.Point(0, 0)
+        Me.pnlBrowser.Name = "pnlBrowser"
+        Me.pnlBrowser.Size = New System.Drawing.Size(1016, 610)
+        Me.pnlBrowser.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 510)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1016, 100)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(416, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "MSDN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1016, 610)
+        Me.Controls.Add(Me.pnlBrowser)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.pnlBrowser.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents pnlBrowser As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
 End Class
--- Cef Sample/Form1.vb
+++ Cef Sample/Form1.vb
@@ -1,3 +1,19 @@
-﻿Public Class Form1
+﻿Imports CefSharp.WinForms
+Public Class Form1
 
+    Private ReadOnly _browser As ChromiumWebBrowser
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        _browser = New ChromiumWebBrowser("http://google.com")
+        pnlBrowser.Controls.Add(_browser)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        _browser.Load("https://msdn.com")
+    End Sub
 End Class
