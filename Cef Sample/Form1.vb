b3b623463d4fb085e0086713e8901f5d0ef3f82c
Jay Asbury <vbjay.net@gmail.com>
2017-09-03 03:05:57 -0400

    Shutdown browser

--- Cef Sample/Form1.vb
+++ Cef Sample/Form1.vb
@@ -16,4 +16,9 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         _browser.Load("https://msdn.com")
     End Sub
+
+    Private Sub Form1_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+        _browser.Dispose()
+        CefSharp.Cef.Shutdown()
+    End Sub
 End Class
